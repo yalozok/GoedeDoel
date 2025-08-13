d35200aaa96d5495d818d2fce17df5b1f212336a
yalozok <olga.kozmovskaia@yandex.ru>
2025-08-13 21:44:34 +0200

    Failed test

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -1,22 +1,27 @@
 package be.vdab.goededoel;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class GoedeDoelTest {
-	private static final String NAAM = "CliniClowns";
-	private GoedeDoel doel;
+    private static final String NAAM = "CliniClowns";
+    private GoedeDoel doel;
 
-	@BeforeEach
-	void beforeEach() {
-		doel = new GoedeDoel(NAAM);
-	}
+    @BeforeEach
+    void beforeEach() {
+        doel = new GoedeDoel(NAAM);
+    }
 
-	@Test
-	void getNaam() {
-		assertThat(doel.getNaam()).isEqualTo(NAAM);
-	}
+    @Test
+    void getNaam() {
+        assertThat(doel.getNaam()).isEqualTo(NAAM);
+    }
+
+    @Test
+    void eenNieuwDoelHeeftNogGeenOpbrengst() {
+        assertThat(doel.getOpbrengst()).isZero();
+    }
 
 }
