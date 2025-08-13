dd4d2d510d39e7ac4753f6530fc1ae3adc78555e
yalozok <olga.kozmovskaia@yandex.ru>
2025-08-13 21:56:40 +0200

    Add Jacoco plugin configuration and override equals in GoedeDoel class

--- src/main/java/be/vdab/goededoel/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -3,18 +3,23 @@ package be.vdab.goededoel;
 import java.math.BigDecimal;
 
 public class GoedeDoel {
-	private final String naam;
-	private BigDecimal opbrengst = BigDecimal.ZERO;
+    private final String naam;
+    private BigDecimal opbrengst = BigDecimal.ZERO;
 
-	public GoedeDoel(String naam) {
-		this.naam = naam;
-	}
+    public GoedeDoel(String naam) {
+        this.naam = naam;
+    }
 
-	public String getNaam() {
-		return naam;
-	}
+    public String getNaam() {
+        return naam;
+    }
 
-	public BigDecimal getOpbrengst() {
-		return opbrengst;
-	}
+    public BigDecimal getOpbrengst() {
+        return opbrengst;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return object instanceof GoedeDoel ander && naam.equalsIgnoreCase(ander.naam);
+    }
 }
